b602e059f5496d144288301287f10bd8f9220677
miuser <miuser@email.com>
2023-01-24 20:02:15 +0800

    获取在线状态

--- app/src/main/java/com/github/makewheels/solar_water_android/MainActivity.java
+++ app/src/main/java/com/github/makewheels/solar_water_android/MainActivity.java
@@ -1,15 +1,20 @@
 package com.github.makewheels.solar_water_android;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alibaba.fastjson2.JSONObject;
+
 import cn.hutool.http.HttpUtil;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv_device_status;
+    private TextView tv_timestamp;
+
     private Button c_stop;
     private Button c_test;
     private Button c_10s;
@@ -25,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        c_stop = findViewById(R.id.c_stop);
+        tv_device_status = findViewById(R.id.tv_device_status);
+        tv_timestamp = findViewById(R.id.tv_timestamp);
+
         c_test = findViewById(R.id.c_test);
         c_10s = findViewById(R.id.c_10s);
         c_15s = findViewById(R.id.c_15s);
@@ -34,42 +41,56 @@ public class MainActivity extends AppCompatActivity {
         c_10m = findViewById(R.id.c_10m);
         c_15m = findViewById(R.id.c_15m);
         addClickListeners();
+
+        loadDeviceStatus();
     }
 
     private void addClickListeners() {
-        String baseUrl = "https://http-solar-ibtoyhpgjm.cn-beijing.fcapp.run?connectTimeLengthInSeconds=";
+        String connectBaseUrl = "https://http-connect-solar-oxjgbdfmab.cn-beijing.fcapp.run?connectTimeLengthInMilliSeconds=";
         c_stop.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 0);
+            HttpUtil.get(connectBaseUrl + 0);
             runOnUiThread(() -> Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show());
         }).start());
         c_test.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 1500);
+            HttpUtil.get(connectBaseUrl + 1500);
             runOnUiThread(() -> Toast.makeText(this, "1.5秒", Toast.LENGTH_SHORT).show());
         }).start());
         c_10s.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 10000);
+            HttpUtil.get(connectBaseUrl + 10000);
             runOnUiThread(() -> Toast.makeText(this, "10秒", Toast.LENGTH_SHORT).show());
         }
         ).start());
         c_15s.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 15000);
+            HttpUtil.get(connectBaseUrl + 15000);
             runOnUiThread(() -> Toast.makeText(this, "15秒", Toast.LENGTH_SHORT).show());
         }).start());
         c_1m.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 60000);
+            HttpUtil.get(connectBaseUrl + 60000);
             runOnUiThread(() -> Toast.makeText(this, "1分钟", Toast.LENGTH_SHORT).show());
         }).start());
         c_5m.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 300000);
+            HttpUtil.get(connectBaseUrl + 300000);
             runOnUiThread(() -> Toast.makeText(this, "5分钟", Toast.LENGTH_SHORT).show());
         }).start());
         c_10m.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 600000);
+            HttpUtil.get(connectBaseUrl + 600000);
             runOnUiThread(() -> Toast.makeText(this, "10分钟", Toast.LENGTH_SHORT).show());
         }).start());
         c_15m.setOnClickListener(view -> new Thread(() -> {
-            HttpUtil.get(baseUrl + 900000);
+            HttpUtil.get(connectBaseUrl + 900000);
             runOnUiThread(() -> Toast.makeText(this, "15分钟", Toast.LENGTH_SHORT).show());
         }).start());
+    }
+
+    private void loadDeviceStatus() {
+        String url = "https://http-gee-status-solar-uucjvfcrmw.cn-beijing.fcapp.run";
+        new Thread(() -> {
+
+            JSONObject jsonObject = JSONObject.parseObject(HttpUtil.get(url));
+            runOnUiThread(() -> {
+                tv_device_status.setText("在线状态：" + jsonObject.getString("deviceStatus"));
+                tv_timestamp.setText("最后在线时间："+ jsonObject.getString("timestampString"));
+            });
+        });
     }
 }
